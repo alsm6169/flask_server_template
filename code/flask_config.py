225000c31bb197d5ec57e40e9c0f03cb6e9e1dfb
alsm6169 <alsm6169@gmail.com>
2020-06-05 23:27:12 +0200

    removed ORM (Object Relationship Mapping) code

--- code/flask_config.py
+++ code/flask_config.py
@@ -7,7 +7,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    print(f'SQLALCHEMY_DATABASE_URI: {SQLALCHEMY_DATABASE_URI}')
+    # print(f'SQLALCHEMY_DATABASE_URI: {SQLALCHEMY_DATABASE_URI}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
