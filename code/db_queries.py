5c2e71d140513645bd5f6b51723d44ed35226dd4
alsm6169 <alsm6169@gmail.com>
2020-06-05 23:10:46 +0200

    removed ORM (Object Relationship Mapping) code

--- code/db_queries.py
+++ code/db_queries.py
@@ -3,48 +3,13 @@ import pandas as pd
 from marshmallow import Schema, fields, validates, validate, post_load, ValidationError
 
 from flask_init import db_obj
-from db_orm import Actor, ActorSchema
 
-def get_all_actors_df():
-    try:
-        actor_df = pd.read_sql(db_obj.session.query(Actor).statement, db_obj.session.bind)
-        # print(actor_df.shape)
-        # print(actor_df.head())
-        return actor_df
-    except psycopg2.DatabaseError as error:
-        print(f'get_all_film_df: {error.pgcode}, {error}')
-        raise RuntimeError('DB Processing Error: ' + str(error))
-    except ValidationError as error:
-        print(f'get_all_film_df: {error}')
-        raise RuntimeError('Invalid Request Parameter: ' + str(error))
-
-
-def get_all_actors_json():
-    try:
-        actors = Actor.query.all()
-        # for actor in actors:
-        #     print('actor.last_name: ', actor.last_name, ', actor.first_name: ', actor.first_name)
-        # return actors
-        actor_schema_obj = ActorSchema(many = True)
-        result = actor_schema_obj.dump(actors)
-        # print('result: ', result)
-        return result
-    except psycopg2.DatabaseError as error:
-        print(f'get_all_film_df: {error.pgcode}, {error}')
-        raise RuntimeError('DB Processing Error: ' + str(error))
-    except ValidationError as error:
-        print(f'get_all_film_df: {error}')
-        raise RuntimeError('Invalid Request Parameter: ' + str(error))
-
-
-def get_all_film_df():
+def get_all_films():
     try:
         query = '''
         SELECT * FROM film
         '''
         film_df = pd.read_sql(query, db_obj.session.bind)
-        # print(film_df.shape)
-        # print (film_df.head ())
         return film_df
     except psycopg2.DatabaseError as error:
         print(f'get_all_film_df: {error.pgcode}, {error}')
@@ -53,11 +18,6 @@ def get_all_film_df():
         print(f'get_all_film_df: {error}')
         raise RuntimeError('Invalid Request Parameter: ' + str(error))
 
-
-# use this style if there are many parameters
-# class FilmInput:
-#     def __init__(self, film_name):
-#         self.film_name = film_name
 
 '''To validate the input parameters'''
 def some_custom_check(data):
@@ -69,16 +29,9 @@ class TitleSchema(Schema):
     title = fields.Str(required=True, validate=[validate.Length(min=1, max=50), some_custom_check])
 
 
-    # use this style if there are many parameters, uncomment FilmInput above
-    # @post_load
-    # def create_film_input_obj(self, data, **kwargs):
-    #    return FilmInput(**data)
-
-def get_film_info_df(request):
+def get_film_info(request):
     try:
         title_schema_obj = TitleSchema()
-        # use this style if there are many parameters
-        # film_in_obj = title_schema_obj.load(request.args)
         title_schema_obj.load(request.args)
         # verification passed, hence code comes here else it would have gone to exception
         title = request.args['title']
@@ -88,8 +41,6 @@ def get_film_info_df(request):
         WHERE title = %(title)s
         '''
         actor_df = pd.read_sql(sql=query, params={'title': title}, con=db_obj.session.bind)
-        # print(actor_df.shape)
-        # print (actor_df.head ())
         return actor_df
     except psycopg2.DatabaseError as error:
         print(f'get_all_film_df: {error.pgcode}, {error}')
@@ -113,8 +64,6 @@ def get_film_actors(request):
         WHERE f.title = %(title)s
         '''
         actor_df = pd.read_sql(sql=query, params={'title': title}, con=db_obj.session.bind)
-        # print(actor_df.shape)
-        # print (actor_df.head ())
         return actor_df
     except psycopg2.DatabaseError as error:
         print(f'get_all_film_df: {error.pgcode}, {error}')
@@ -122,5 +71,3 @@ def get_film_actors(request):
     except ValidationError as error:
         print(f'get_all_film_df: {error}')
         raise RuntimeError('Invalid Request Parameter: ' + str(error))
-
-
--- code/flask_routes.py
+++ code/flask_routes.py
@@ -15,11 +15,11 @@ def bad_request():
     """Bad request."""
     return make_response(jsonify({'Error': 'BAD REQUEST'}, 400))
 
-@app.route('/module/v01/functions/actor_list_alchemy_df', methods=['GET'])
-def get_actor_list_alchemy_df():
+@app.route('/module/v01/functions/film_list', methods=['GET'])
+def get_film_list():
     try:
-        print('inside get_actor_list_alchemy_df')
-        actor_df = db_queries.get_all_actors_df()
+        print('inside get_film_list')
+        actor_df = db_queries.get_all_films()
         response_code = 200
         response_msg = actor_df.to_json(orient='records')
     except RuntimeError as err:
@@ -27,37 +27,11 @@ def get_actor_list_alchemy_df():
         response_msg = jsonify(str(err))
     return make_response(response_msg, response_code)
 
-
-@app.route('/module/v01/functions/actor_list_alchemy_json', methods=['GET'])
-def get_actor_list_alchemy_json():
+@app.route('/module/v01/functions/film_info', methods=['GET'])
+def get_film_info():
     try:
-        print('inside get_actor_list_alchemy_json')
-        actors = db_queries.get_all_actors_json()
-        response_code = 200
-        # response_msg = jsonify({'STATUS' : 'WORKED INTERNALLY'})
-        response_msg = jsonify(actors)
-    except RuntimeError as err:
-        response_code = 700  # err.code
-        response_msg = jsonify(str(err))
-    return make_response(response_msg, response_code)
-
-@app.route('/module/v01/functions/film_list_rawsql_df', methods=['GET'])
-def get_film_list_rawsql_df():
-    try:
-        print('inside get_film_list_rawsql_df')
-        actor_df = db_queries.get_all_film_df()
-        response_code = 200
-        response_msg = actor_df.to_json(orient='records')
-    except RuntimeError as err:
-        response_code = 700  # err.code
-        response_msg = jsonify(str(err))
-    return make_response(response_msg, response_code)
-
-@app.route('/module/v01/functions/film_info_rawsql_df', methods=['GET'])
-def get_film_info_rawsql_df():
-    try:
-        print('inside get_film_info_rawsql_df')
-        actor_df = db_queries.get_film_info_df(request)
+        print('inside get_film_info')
+        actor_df = db_queries.get_film_info(request)
         response_code = 200
         response_msg = actor_df.to_json(orient='records')
     except RuntimeError as err:
@@ -65,10 +39,10 @@ def get_film_info_rawsql_df():
         response_msg = jsonify(str(err))
     return make_response(response_msg, response_code)
 
-@app.route('/module/v01/functions/film_actors_rawsql_df', methods=['GET'])
-def get_film_actors_rawsql_df():
+@app.route('/module/v01/functions/film_actors', methods=['GET'])
+def get_film_actors():
     try:
-        print('inside get_film_actors_rawsql_df')
+        print('inside get_film_actors')
         actor_df = db_queries.get_film_actors(request)
         response_code = 200
         response_msg = actor_df.to_json(orient='records')
