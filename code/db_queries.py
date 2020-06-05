c95dfe82458cd490d84c0eee69ac1c60ac1b6a0a
alsm6169 <alsm6169@gmail.com>
2020-06-05 12:15:55 +0200

    re-structured the code more in line with the framework and more modularized

--- code/db_queries.py
+++ code/db_queries.py
@@ -1,30 +1,8 @@
 import psycopg2
 import pandas as pd
-import marshmallow as ma
 
 from flask_init import db_obj
-
-# DB sample source: https://www.postgresqltutorial.com/postgresql-sample-database/
-class Actor(db_obj.Model):
-    __tablename__ = 'actor'
-    actor_id = db_obj.Column(db_obj.Integer, primary_key=True)
-    first_name = db_obj.Column(db_obj.String(50))
-    last_name = db_obj.Column(db_obj.String(120))
-    last_update = db_obj.Column(db_obj.DateTime)
-
-    def __init__(self, first_name=None, last_name=None):
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def __repr__(self):
-        return '<User %r>' % (self.first_name)
-
-class ActorSchema(ma.Schema):
-    class Meta:
-        fields = ("actor_id", "first_name", "last_name")
-        # fields = ("actor_id", "first_name", "last_name", "last_update")
-        # exclude = ("last_update")
-
+from tbl_actor import Actor, ActorSchema
 
 def get_all_actors_df():
     try:
@@ -47,3 +25,29 @@ def get_all_actors_json():
         return result
     except psycopg2.DatabaseError as error:
         print(f'get_all_actors: {error.pgcode}, {error}')
+
+
+def get_all_film_df():
+    try:
+        query = '''
+        select * from film
+        '''
+        film_df = pd.read_sql(query, db_obj.session.bind)
+        # print(film_df.shape)
+        # print (film_df.head ())
+        return film_df
+    except psycopg2.DatabaseError as error:
+        print(f'get_all_film_df: {error.pgcode}, {error}')
+
+'''TODO: Work in Progress'''
+def get_film_actors_df():
+    try:
+        query = '''
+        select * from film
+        '''
+        actor_df = pd.read_sql(query, db_obj.session.bind)
+        # print(actor_df.shape)
+        # print (actor_df.head ())
+        return actor_df
+    except psycopg2.DatabaseError as error:
+        print(f'get_all_film_df: {error.pgcode}, {error}')
--- code/flask_routes.py
+++ code/flask_routes.py
@@ -1,9 +1,8 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from flask import current_app as app
 from marshmallow import ValidationError
 
-import db_pandas
-import db_sqlalchemy
+import db_queries
 
 
 @app.errorhandler(404)
@@ -21,7 +20,7 @@ def bad_request():
 def get_actor_list_alchemy_df():
     try:
         print('inside get_actor_list_alchemy_df')
-        actor_df = db_sqlalchemy.get_all_actors_df()
+        actor_df = db_queries.get_all_actors_df()
         response_code = 200
         response_msg = actor_df.to_json(orient='records')
     except ValidationError as err:
@@ -37,7 +36,7 @@ def get_actor_list_alchemy_df():
 def get_actor_list_alchemy_json():
     try:
         print('inside get_actor_list_alchemy_json')
-        actors = db_sqlalchemy.get_all_actors_json()
+        actors = db_queries.get_all_actors_json()
         response_code = 200
         # response_msg = jsonify({'STATUS' : 'WORKED INTERNALLY'})
         response_msg = jsonify(actors)
@@ -49,11 +48,26 @@ def get_actor_list_alchemy_json():
         response_msg = jsonify(err.messages)
     return make_response(response_msg, response_code)
 
-@app.route('/module/v01/functions/actor_list_rawsql_df', methods=['GET'])
-def get_actor_list_rawsql_df():
+@app.route('/module/v01/functions/film_list_rawsql_df', methods=['GET'])
+def get_film_list_rawsql_df():
     try:
-        print('inside get_actor_list_rawsql_df')
-        actor_df = db_pandas.get_all_actors_df()
+        print('inside get_film_list_rawsql_df')
+        actor_df = db_queries.get_all_film_df()
+        response_code = 200
+        response_msg = actor_df.to_json(orient='records')
+    except ValidationError as err:
+        response_code = err.code
+        response_msg = jsonify(err.messages)
+    except RuntimeError as err:
+        response_code = err.code
+        response_msg = jsonify(err.messages)
+    return make_response(response_msg, response_code)
+
+@app.route('/module/v01/functions/film_actor_rawsql_df', methods=['GET'])
+def get_film_actors_rawsql_df():
+    try:
+        print('inside get_film_actors_rawsql_df')
+        actor_df = db_queries.get_film_actors_df(request.args)
         response_code = 200
         response_msg = actor_df.to_json(orient='records')
     except ValidationError as err:
