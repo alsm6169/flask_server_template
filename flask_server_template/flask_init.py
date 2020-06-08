8bcc5d7ad681d20e75086d6711be3ae92a6cf268
alsm6169 <alsm6169@gmail.com>
2020-06-08 11:07:14 +0200

    made flask initialization more configurable

--- flask_server_template/flask_init.py
+++ flask_server_template/flask_init.py
@@ -2,27 +2,33 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import ValidationError, Schema, fields, validate
 import logging
+import os
 
 db_obj = SQLAlchemy()
 
 log = logging.getLogger('pythonLogger') # This handler comes from config>logger.conf
+# https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/
 
-def create_app(flask_config):
+def create_app(flask_config=None):
     """Construct the core application."""
     log.info(flask_config)
     app = Flask(__name__, instance_relative_config=False)
 
-    '''
-    Code line --> 
-    app.config.from_object('flask_config.DevelopmentConfig')
-    goes to file flask_config.py and reads configuration class DevelopmentConfig
-    '''
-    app.config.from_object(flask_config)
+    if flask_config is None:
+        app.config['DEBUG'] = False
+        if 'SQLALCHEMY_DATABASE_URI' not in os.environ:
+            raise RuntimeError('Environment variables not set')
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_ECHO'] = False
+    else:
+        app.config.from_object(flask_config)
 
     db_obj.init_app(app)
 
     with app.app_context():
-        import flask_routes  # Import routes
+        # Import routes
+        import flask_routes
         return app
 
 
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from flask_init import create_app
 print('platform.python_version(): ', platform.python_version())
 
 if len(sys.argv) != 2:
-    run_config_file = Path('../config/') / main_config.RUN_PARAMS_DEFAULT_FILENM
+    run_config_file = Path('config/') / main_config.RUN_PARAMS_DEFAULT_FILENM
 else:
     run_config_file = Path(sys.argv[1])
 
@@ -31,9 +31,13 @@ log.info('__name__: ' + __name__)
 
 # Flask Code - BEGIN
 """App entry point."""
-app = create_app(main_config.run_conf_data['FLASK_CONFIG'])
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+try:
+    app = create_app(main_config.run_conf_data['FLASK_CONFIG'])
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0')
+except RuntimeError as err:
+    log.error(str(err))
+    exit(1)
 # Flask Code - END
 
 # Main - END
